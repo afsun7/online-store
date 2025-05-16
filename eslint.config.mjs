194c362faf7b398025e9d0d17db0fae34f70e13b
afsun7 <omidi.afsaneh7@gmail.com>
2025-05-16 15:40:27 +0330

    .

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,35 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: ["react-refresh"],
+    rules: {
+      "no-console": "error",
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "jsx-quotes": ["error", "prefer-single"],
+      "react/jsx-no-literals": "error",
+      "no-restricted-imports": [
+        "error",
+        {
+          name: "next/link",
+          message: "Please import from `@/i18n/routing` instead.",
+        },
+        {
+          name: "next/navigation",
+          importNames: [
+            "redirect",
+            "permanentRedirect",
+            "useRouter",
+            "usePathname",
+          ],
+          message: "Please import from `@/i18n/routing` instead.",
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
